6919bddd391c8f741ddd67c127c25129e2921024
juergenbarth <87374371+juergenbarth@users.noreply.github.com>
2025-11-25 14:24:56 +0100

    Update print_today_task_list.py

--- n8n/task_management/print_today_task_list.py
+++ n8n/task_management/print_today_task_list.py
@@ -7,7 +7,7 @@ printer_ip = sys.argv[1]
 title = sys.argv[2].replace("<br>", '\n')
 weather = sys.argv[3].replace("<br>", '\n')
 tasks = sys.argv[4].replace("<br>", '\n')
-motd = sys.argv[5]
+motd = sys.argv[5].replace("<br>", '\n')
 
 """ Seiko Epson Corp. Receipt Printer M129 Definitions (EPSON TM-T20IV) """
 Epson = printer.Network(printer_ip, profile="TM-T20II")
