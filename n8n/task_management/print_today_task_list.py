92b4aec3f28e41a9dd79bfed3f418930ee8e4b64
juergenbarth <87374371+juergenbarth@users.noreply.github.com>
2025-11-28 08:15:09 +0100

    Update print_today_task_list.py

--- n8n/task_management/print_today_task_list.py
+++ n8n/task_management/print_today_task_list.py
@@ -6,13 +6,14 @@ from escpos import *
 printer_ip = sys.argv[1]
 title = sys.argv[2].replace("<br>", '\n')
 weather = sys.argv[3].replace("<br>", '\n')
-tasks = sys.argv[4].replace("<br>", '\n')
+tasks = sys.argv[4].replace("<br>,", '\n').replace("<br>", '\n')
 motd = sys.argv[5].replace("<br>", '\n')
 
 """ Seiko Epson Corp. Receipt Printer M129 Definitions (EPSON TM-T20IV) """
 Epson = printer.Network(printer_ip, profile="TM-T20II")
 Epson.set(align='center', double_width=True, double_height=True)
 Epson.textln(title)
+Epson.set(align='center', normal_textsize=True, bold=True)
 Epson.textln(weather)
 Epson.textln("----------")
 Epson.set(align='left', normal_textsize=True)
